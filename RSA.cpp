05a54ecabdaa1badeaf29fa5bc8dc46f1bc67d81
Nayeem <52297907+nayeem-bd@users.noreply.github.com>
2023-01-13 00:43:05 +0600

    Update RSA.cpp

--- RSA.cpp
+++ RSA.cpp
@@ -20,8 +20,9 @@ ll modPower(ll x,ll y,ll p){
     ll res=1;
     x=x%p;
     while(y>0){
-        if(y&1)res=(res*x)%p;
-        y=y>>1;x=(x*x)%p;
+        if(y%2==1)res=(res*x)%p;
+        y/=2;
+        x=(x*x)%p;
     }
     return res;
 }
