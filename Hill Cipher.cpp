f4cff19a14bbd7b8fbc02e69f373636bf64a846e
Nayeem <52297907+nayeem-bd@users.noreply.github.com>
2023-02-24 02:12:12 +0600

    encryption and decryption for keysize 4 and 9

--- Hill Cipher.cpp
+++ Hill Cipher.cpp
@@ -1,7 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-float keyMatrix[3][3],cipherMatrix[3],messageMatrix[3],inverseKeyMatrix[3][3],plainMatrix[3];
+int keyMatrix[3][3],cipherMatrix[3],messageMatrix[3],inverseKeyMatrix[3][3],plainMatrix[3];
+int x,y;
+int gcd(int a,int b)
+{
+    if(b==0){
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1,y1;
+    int gc = gcd(b,a%b);
+    x = y1;
+    y = x1 - (a/b)*y1;
+    return gc;
+}
 
 void matrix_gen(string& key,int keysize)
 {
@@ -19,6 +33,7 @@ void matrix_gen(string& key,int keysize)
 }
 
 string encyptionTotal(string& plainText,string& key,int keysize){
+    if(key.size()!=keysize*keysize) return "Key size doesn't match";
     for(int i=0;i<(plainText.size()%keysize);i++){
         plainText+='X';
     }
@@ -42,31 +57,53 @@ string encyptionTotal(string& plainText,string& key,int keysize){
 }
 
 bool find_inverse_2(){
-
-
+    int det = keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[0][1] * keyMatrix[1][0];
+    if(det==0){
+        cout<<"Inverse is not possible: "<<endl;
+        return false;
+    }
+    int gc = gcd(det,26);
+    if(gc!=1){
+        cout<<"Determinant: "<<det<<endl;
+        cout<<"Gcd is not 1"<<endl;
+        return false;
+    }
+    //here x is modular multiplicative inverse which is declared globally
+    inverseKeyMatrix[0][0] = (keyMatrix[1][1]*gc)%26;
+    inverseKeyMatrix[0][1] = (26-keyMatrix[0][1]*gc)%26;
+    inverseKeyMatrix[1][0] = (26-keyMatrix[1][0]*gc)%26;
+    inverseKeyMatrix[1][1] = (keyMatrix[0][0]*gc)%26;
     return true;
 }
 
 bool find_inverse_3(){
-    float det = keyMatrix[0][0] * (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1]) - keyMatrix[0][1] * (keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0])+ keyMatrix[0][2] * (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0]);
+    int det = keyMatrix[0][0] * (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1]) - keyMatrix[0][1] * (keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0])+ keyMatrix[0][2] * (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0]);
     if (det == 0) {
         cout << "Matrix is singular and has no inverse." << endl;
         return false;
     }
-    inverseKeyMatrix[0][0] = (keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1]) / det;
-    inverseKeyMatrix[0][1] = -(keyMatrix[0][1] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][1]) / det;
-    inverseKeyMatrix[0][2] = (keyMatrix[0][1] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][1]) / det;
-    inverseKeyMatrix[1][0] = -(keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0]) / det;
-    inverseKeyMatrix[1][1] = (keyMatrix[0][0] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][0]) / det;
-    inverseKeyMatrix[1][2] = -(keyMatrix[0][0] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][0]) / det;
-    inverseKeyMatrix[2][0] = (keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0]) / det;
-    inverseKeyMatrix[2][1] = -(keyMatrix[0][0] * keyMatrix[2][1] - keyMatrix[0][1] * keyMatrix[2][0]) / det;
-    inverseKeyMatrix[2][2] = (keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[0][1] * keyMatrix[1][0]) / det;
+    int gc = gcd(det,26);
+    if(gc!=1){
+        cout<<"Determinant: "<<det<<endl;
+        cout<<"Gcd is not 1"<<endl;
+        return false;
+    }
+    //here x is modular multiplicative inverse which is declared globally
+    inverseKeyMatrix[0][0] = ((keyMatrix[1][1] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][1])*x)%26 ;
+    inverseKeyMatrix[0][1] = (26-(keyMatrix[0][1] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][1])*x)%26;
+    inverseKeyMatrix[0][2] = ((keyMatrix[0][1] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][1])*x)%26;
+    inverseKeyMatrix[1][0] = (26-(keyMatrix[1][0] * keyMatrix[2][2] - keyMatrix[1][2] * keyMatrix[2][0])*x)%26;
+    inverseKeyMatrix[1][1] = ((keyMatrix[0][0] * keyMatrix[2][2] - keyMatrix[0][2] * keyMatrix[2][0])*x)%26;
+    inverseKeyMatrix[1][2] = (26-(keyMatrix[0][0] * keyMatrix[1][2] - keyMatrix[0][2] * keyMatrix[1][0])*x)%26;
+    inverseKeyMatrix[2][0] = ((keyMatrix[1][0] * keyMatrix[2][1] - keyMatrix[1][1] * keyMatrix[2][0])*x)%26;
+    inverseKeyMatrix[2][1] = (26-(keyMatrix[0][0] * keyMatrix[2][1] - keyMatrix[0][1] * keyMatrix[2][0])*x)%26;
+    inverseKeyMatrix[2][2] = ((keyMatrix[0][0] * keyMatrix[1][1] - keyMatrix[0][1] * keyMatrix[1][0])*x)%26;
     return true;
 }
 
 
 string decryption(string& cipherText,string& key,int keysize){
+    if(key.size()!=keysize*keysize) return "Key size doesn't match";
     string plainText;
     matrix_gen(key,keysize);
     bool inversePossible = false;
@@ -77,8 +114,8 @@ string decryption(string& cipherText,string& key,int keysize){
         inversePossible=find_inverse_3();
     }
     if(!inversePossible)return "Inverse Not Possible";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
+    for(int i=0;i<keysize;i++){
+        for(int j=0;j<keysize;j++){
             cout<<inverseKeyMatrix[i][j]<<" ";
         }
         cout<<endl;
@@ -86,26 +123,20 @@ string decryption(string& cipherText,string& key,int keysize){
     for(int i=0;i<cipherText.size();i+=keysize){
         for(int j=0;j<keysize;j++){
             messageMatrix[j]=cipherText[i+j]-'A';
-            //cout<<messageMatrix[j]<<" ";
         }
         for(int j=0;j<keysize;j++){
             plainMatrix[j]=0;
             for(int k=0;k<keysize;k++){
-            	cout<<fmod(inverseKeyMatrix[j][k],26)<<" "<<messageMatrix[k]<<endl;
                 plainMatrix[j]+=inverseKeyMatrix[j][k]*messageMatrix[k];
             }
         }
         for(int j=0;j<keysize;j++){
-        	cout<<plainMatrix[j]<<endl;
             plainText+=(char)(fmod(plainMatrix[j],26)+'A');
         }
     }
 
     return plainText;
 }
-
-
-
 
 void solve(int keysize){
     string text,key,cipherText,output;
@@ -116,15 +147,19 @@ void solve(int keysize){
         if(choice==1){
             cout<<"Enter PlainText: ";
             cin>>text;
-            cout<<"Enter key (length="<<keysize<<"): ";
+            cout<<"Enter key (length="<<keysize*keysize<<"): ";
             cin>>key;
+            transform(text.begin(),text.end(),text.begin(),::toupper);
+            transform(key.begin(),key.end(),key.begin(),::toupper);
             cipherText = encyptionTotal(text,key,keysize);
             cout<<cipherText<<endl;
         }else if(choice==2){
             cout<<"Enter CipherText: ";
             cin>>text;
-            cout<<"Enter key (length="<<keysize<<"): ";
+            cout<<"Enter key (length="<<keysize*keysize<<"): ";
             cin>>key;
+            transform(text.begin(),text.end(),text.begin(),::toupper);
+            transform(key.begin(),key.end(),key.begin(),::toupper);
             output = decryption(text,key,keysize);
             cout<<output<<endl;
         }else{
