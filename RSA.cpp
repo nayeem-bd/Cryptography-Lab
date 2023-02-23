b4e8e6f24942d779c4cab26163d6bcd8c479fc05
Nayeem <52297907+nayeem-bd@users.noreply.github.com>
2023-02-24 02:24:35 +0600

    input and output using file for RSA
    message must given in message.txt file

--- RSA.cpp
+++ RSA.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 #define ll long long
 
-ll gcd(ll a,ll b,ll *x,ll *y )
+ll x,y;
+
+ll gcd(ll a,ll b)
 {
     if(b==0){
-        *x = 1;
-        *y = 0;
+        x = 1;
+        y = 0;
         return a;
     }
     ll x1,y1;
-    ll gc = gcd(b,a%b,&x1,&y1);
-    *x = y1;
-    *y = x1 - (a/b)*y1;
+    ll gc = gcd(b,a%b);
+    x = y1;
+    y = x1 - (a/b)*y1;
     return gc;
 }
 
@@ -57,7 +59,7 @@ void decryption(ll d,ll n)
 int main()
 {
     while(1){
-        ll x,y,phi,e,p,q,d;
+        ll phi,e,p,q,d;
         cout<<"Enter p and q: "<<endl;
         cin>>p>>q;
         ll n = p*q;
@@ -68,7 +70,7 @@ int main()
         ll gc = 0;
         while(gc!=1){
             cin>>e;
-            gc = gcd(e,phi,&x,&y);
+            gc = gcd(e,phi);
             if(gc!=1)cout<<"e and phi are not co prime"<<endl;
         }
         d = ((x%phi)+phi)%phi;
